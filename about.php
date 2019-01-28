292bc9e695a40a41a5bafc8bf0da159017bfe941
maintenantcjoey <legrand.jessica45@gmail.com>
2019-01-28 14:13:23 +0100

    suite front

---
+++ about.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>About</title>
+        <link rel="stylesheet" href="src/css/base.css">
+
+    </head>
+
+        <body class="bodyAbout">
+
+        <?php include ("base.php") ?>
+
+        <div class="container-fluid d-flex justify-content-center">
+            <h2>About</h2>
+        </div>
+
+        <div class="container" id="about" >
+            <p>Nouvelle venue sur Paris, j'ai intensivement le développement PHP durant 5mois à la <a href="https://wildcodeschool.fr/" target="_blank" id="wild"> Wild Code School</a>, <br>
+                avant ça j'ai été vendeuse, en prêt-à-porter principalement. Et encore avant, j'était étudiante en droit, <br>
+                mais le formalisme et la rigidité du milieu m'a poussé à m'éloigner de ce domaine et à cherche ma voix ailleurs.<br><br>
+                Le développement, en son sens général, laisse place à toutes créativités dans un cadre structuré.<br>
+                Et ça! C'est génial!<br>
+                De plus il offre tant de possibilités d'évolutions personnelles! Tout est possible.
+                Par exemple, l'UI/UX m'interesse beaucoup, je pense que l'on perd trop vite de vue les utilisateurs, surtout en tant que débutant. Et puis c'est un sujet passionnant.<br>
+                Je m'interesse aussi de près à la gestion de projet. C'est un poste fondamental dans une équipe et le lien avec le client me plait.
+            </p>
+
+            <p>Ce site a été entièrement codé par mes petits doigts avec une architecture MVC. <br>
+                Je me ferai une joie de vous en parler et de vous montrer plus en détail le code si vous me donner la chance de faire mes preuves pendant un entretien.
+            </p>
+            <br>
+                <div id="bordel">
+                    <p>Et sinon:<br>
+                    My favorite TV show: <a href="https://www.google.com/search?client=ubuntu&channel=fs&q=brooklyn+99&ie=utf-8&oe=utf-8" class="about">Brooklyn Nine-Nine</a><br>
+                    My favorite Icecream: Pistachio<br>
+                    My favorite Miyazaki: <a href="https://www.google.com/search?client=ubuntu&channel=fs&q=chihori&ie=utf-8&oe=utf-8" class="about">Chihiro</a><br>
+                    My cat's name: Chacha<br>
+                ...</p>
+                </div>
+        </div>
+
+
+
+
+
+
+        </body>
+</html>
---
+++ base.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Joey</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <link rel="stylesheet" href="src/css/base.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Bangers|Dosis:300" rel="stylesheet">
+    </head>
+
+    <body class="bodyBase">
+
+        <nav class="navbar navbar-expand-lg">
+
+            <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php"> Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Features/realiations</a>
+                        <div class="dropdown-menu" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 40px, 0px);">
+                            <!-- x-placement="bottom-start" -->
+                            <a class="dropdown-item" href="#">Code</a>
+                            <a class="dropdown-item" href="#">Scrum</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php"> About </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"> Lettre de Motivation </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="curiculum.php"> CV </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    </body>
+</html>
